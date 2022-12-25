bd152e54354c260150b0ab373037e999881b0f55
chivicoko <vicharmonic@gmail.com>
2022-12-25 08:25:18 +0000

    add more conditions

--- script.js
+++ script.js
@@ -5,12 +5,15 @@ document.getElementById('btn').onclick = () => {
     var percent = Math.random() * 100;
     var finalLovePercent = Math.floor(percent) + 1
 
-    if (finalLovePercent >= 70) {
+    if (finalLovePercent >= 80) {
         document.getElementById('lovePercentOutput').innerHTML =
-        "The love between you and " + secondInput + " is: " + finalLovePercent + "%. Your love for each other is deeper than the oceans! 💓";
+        "The love between you and " + secondInput + " is: " + finalLovePercent + "%. Your love for each other is deeper than the oceans! 💓 💪";
+    }else if(finalLovePercent >= 40 && finalLovePercent < 80){
+        document.getElementById('lovePercentOutput').innerHTML =
+        "The love between you and " + secondInput + " is: " + finalLovePercent + "%. It's growing fast! 💓";
     } else{
         document.getElementById('lovePercentOutput').innerHTML =
-        "The love between you and " + secondInput + " is: " + finalLovePercent + "%";
+        "The love between you and " + secondInput + " is: " + finalLovePercent + "%. Needs more nourishment...";
     }
 
 }
